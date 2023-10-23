feb5be67f1cad27db4b9cfbf1692fa6edee6b031
Saket Singh <saketaryanite@gmail.com>
2023-10-23 23:58:03 +0530

    updated

--- server.js
+++ server.js
@@ -8,27 +8,22 @@ const server = http.createServer((req, res) => {
 
   if (method === 'GET') {
     if (url === '/html') {
-      // Serve HTML content
       const htmlContent = fs.readFileSync(path.join(__dirname, './public/coder.html'), 'utf8');
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(htmlContent);
     } else if (url === '/json') {
-      // Serve JSON content
       const jsonContent = require('./public/coder.json');
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(jsonContent, null, 2));
     } else if (url === '/uuid') {
-      // Generate and serve a UUID
       const randomUUID = uuidv4();
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ uuid: randomUUID }));
     } else if (url.startsWith('/status/')) {
-      // Serve a response with the specified status code
       const statusCode = parseInt(url.split('/').pop());
       res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
       res.end(`Response with status code ${statusCode}`);
     } else if (url.startsWith('/delay/')) {
-      // Delay response for the specified seconds
       const delay = parseInt(url.split('/').pop()) * 1000;
       setTimeout(() => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
