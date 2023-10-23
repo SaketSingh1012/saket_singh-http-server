a2163cdedc6c0e34e714a213a7d22d86f1b15f4c
Saket Singh <saketaryanite@gmail.com>
2023-10-23 23:59:48 +0530

    updated

--- app.js
+++ app.js
@@ -6,31 +6,26 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 app.get('/html', (req, res) => {
-  // Serve HTML content
   const htmlContent = fs.readFileSync(path.join(__dirname, './public/coder.html'), 'utf8');
   res.status(200).type('text/html').send(htmlContent);
 });
 
 app.get('/json', (req, res) => {
-  // Serve JSON content
   const jsonContent = require('./public/coder.json');
   res.status(200).json(jsonContent);
 });
 
 app.get('/uuid', (req, res) => {
-  // Generate and serve a UUID
   const randomUUID = uuidv4();
   res.status(200).json({ uuid: randomUUID });
 });
 
 app.get('/status/:statusCode', (req, res) => {
-  // Serve a response with the specified status code
   const statusCode = parseInt(req.params.statusCode);
   res.status(statusCode).send(`Response with status code ${statusCode}`);
 });
 
 app.get('/delay/:delayInSeconds', (req, res) => {
-  // Delay response for the specified seconds
   const delay = parseInt(req.params.delayInSeconds) * 1000;
   setTimeout(() => {
     res.status(200).send(`Response with a ${delay / 1000} second delay`);
